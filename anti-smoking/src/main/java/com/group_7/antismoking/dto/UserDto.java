a7b9c33ca0a30b67c15c619054baceee2c940c2d
TheGnort <ht147652@gmail.com>
2025-05-26 16:22:32 +0700

    Add dto branch

---
+++ anti-smoking/src/main/java/com/group_7/antismoking/dto/UserDto.java
@@ -0,0 +1,12 @@
+package com.group_7.antismoking.dto;
+
+public class UserDto {
+        private Long id;
+        private String fullName;
+        private String email;
+        private String gender;
+
+        
+    }
+
+}
