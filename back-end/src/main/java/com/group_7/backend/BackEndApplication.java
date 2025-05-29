23c89ede3d35115624c6395a7abc2e09981fa9f1
TheGnort <ht147652@gmail.com>
2025-05-29 15:17:55 +0700

    Upload React

--- back-end/src/main/java/com/group_7/backend/BackEndApplication.java
+++ back-end/src/main/java/com/group_7/backend/BackEndApplication.java
@@ -1,13 +1,21 @@
 package com.group_7.backend;
 
+import com.group_7.backend.dto.UserDto;
+import com.group_7.backend.service.impl.UserServiceImp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class BackEndApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackEndApplication.class, args);
+        //SpringApplication.run(BackEndApplication.class, args);
+
+
+
     }
+
 
 }
