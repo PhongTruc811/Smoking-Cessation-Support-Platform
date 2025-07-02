ac989426f8dd94713a16d4e17c75b42cd45d864a
TrucNguyen <phongtruc811@gmail.com>
2025-07-02 14:39:37 +0700

    adjust toast message

--- back-end/src/main/java/com/group_7/backend/service/impl/UserServiceImp.java
+++ back-end/src/main/java/com/group_7/backend/service/impl/UserServiceImp.java
@@ -100,7 +100,7 @@ public class UserServiceImp implements IUserService {
         if (userOpt.isEmpty()) {
             userOpt = userRepository.findByEmail(usernameOrEmail);
         }
-        if (userOpt.isEmpty()) throw new ResourceNotFoundException("User not found");
+        if (userOpt.isEmpty()) throw new ResourceNotFoundException("User does not exist, please check username/email before login 😗");
 
         User user = userOpt.get();
 
